e101f766d80bce53b3b317c5314a5086c5cc8b8c
giselabagu <giselabagu@outlook.com>
2025-11-13 12:16:07 +0100

    actualización SQL

--- INTENTO_FINAL/CREATE_bbdd_musicstream.sql
+++ INTENTO_FINAL/CREATE_bbdd_musicstream.sql
@@ -1,10 +1,20 @@
--- DROP SCHEMA IF EXISTS musicstream;
+DROP SCHEMA IF EXISTS musicstream;
 
 CREATE SCHEMA musicstream;
 
 USE musicstream;
 
 -- TABLE 1
+CREATE TABLE lastfm (
+	artist_name VARCHAR(255) PRIMARY KEY, 	
+    biography TEXT,							
+    listeners INT,							
+    playcount INT,
+    similar_artists VARCHAR(500)
+	);
+
+
+    -- TABLE 2
 CREATE TABLE spotify (	
 	id VARCHAR(40) PRIMARY KEY,
     track_name VARCHAR(150),
@@ -13,15 +23,11 @@ CREATE TABLE spotify (
     genre ENUM('soundtrack', 'k-pop', 'afro', 'electronic'),
     album_type VARCHAR(45),
     release_date DATE,									
-    popularity INT
-	);
-
-    -- TABLE 2
-CREATE TABLE lastfm (
-	artist_name VARCHAR(255) PRIMARY KEY, 	
-    biography TEXT,							
-    listeners INT,							
-    playcount INT,
-    similar_artists VARCHAR(500)
-	);
+    popularity INT,
+    CONSTRAINT fk_spotify_lastfm
+		FOREIGN KEY (artist_name) 
+        REFERENCES lastfm(artist_name)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
  
